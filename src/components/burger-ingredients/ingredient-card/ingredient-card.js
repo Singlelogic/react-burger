26e7668591b285056c67359ea4a1ff6013fe2e3d
Artem <singlelogic@gmail.com>
2023-08-30 15:56:22 +0300

    add counter on card ingredient

--- src/components/burger-ingredients/ingredient-card/ingredient-card.js
+++ src/components/burger-ingredients/ingredient-card/ingredient-card.js
@@ -1,9 +1,10 @@
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './ingredient-card.module.css';
 
 function CardIngredient({ ingredient }) {
   return (
     <div className={styles.card}>
+      <Counter count={1} size="small" />
       <img src={ingredient.image} alt={ingredient.ingredient} />
       <span className={styles.price}>
         <span className="text text_type_digits-default">{ingredient.price}</span>
