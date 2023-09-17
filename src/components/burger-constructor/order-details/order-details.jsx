b23e0a3d9a7229801b01c9fa4d0a36288986368f
Artem <singlelogic@gmail.com>
2023-09-17 20:44:35 +0300

    add loader order

--- src/components/burger-constructor/order-details/order-details.jsx
+++ src/components/burger-constructor/order-details/order-details.jsx
@@ -1,20 +1,34 @@
 import { useSelector } from 'react-redux';
 import { CheckMarkIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Loader } from '../../../ui/loader/loader';
 import styles from './order-details.module.css';
 
 function OrderDetails() {
-  const orderNumber = useSelector(store => store.burgerConstructor.orderNumber);
+  const {
+    sendOrderRequest,
+    sendOrderFailed,
+    orderNumber,
+  } = useSelector(store => store.burgerConstructor);
 
   return (
-    <div className={styles.content}>
-      <p className="text text_type_digits-large">{orderNumber}</p>
-      <p className="text text_type_main-default">Идентификатор заказа</p>
-      <p className={styles.icon}><CheckMarkIcon type="secondary" /></p>
-      <p className="text text_type_main-small">Ваш заказ начали готовить</p>
-      <p className="text text_type_main-small text_color_inactive">
-        Дождитесь готовности на орбитальной станции
-      </p>
-    </div>
+    sendOrderRequest ? (
+      <Loader size="large" />
+    ) : (
+      sendOrderFailed ?
+        <p className="text text_type_main-default">
+          Ошибка отправки заказа!
+        </p>
+      :
+        <div className={styles.content}>
+          <p className="text text_type_digits-large">{orderNumber}</p>
+          <p className="text text_type_main-default">Идентификатор заказа</p>
+          <p className={styles.icon}><CheckMarkIcon type="secondary" /></p>
+          <p className="text text_type_main-small">Ваш заказ начали готовить</p>
+          <p className="text text_type_main-small text_color_inactive">
+            Дождитесь готовности на орбитальной станции
+          </p>
+        </div>
+    )
   )
 }
 
