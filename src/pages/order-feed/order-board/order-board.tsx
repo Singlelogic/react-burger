eb19d4f5ced33cca2ca81f21b635907ce0dc80fb
Artem <singlelogic@gmail.com>
2023-11-03 14:26:50 +0300

    add total and total today orders

--- src/pages/order-feed/order-board/order-board.tsx
+++ src/pages/order-feed/order-board/order-board.tsx
@@ -1,16 +1,31 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+
 import styles from "./order-board.module.css";
+import { getOrderFeedStore } from "../../../utils/store";
 
 
 const OrderBoard = () => {
+  const { ordersFeed: { orders, total, totalToday } } = useSelector(getOrderFeedStore);
+
   return (
     <>
-
       <div className={styles.listOrderStatuses}>
         <div className={`text text_type_main-medium ${styles.statusDone}`}>
           <span>Готовы:</span>
+            {orders && orders.map((order: any) => {
+              if (order.status === 'done') {
+                return <div className={styles.done} key={order._id}>{ order.number }</div>
+              }
+            })}
         </div>
         <div className={`text text_type_main-medium ${styles.statusProgress}`}>
           <span>В работе:</span>
+            {orders && orders.map((order: any) => {
+              if (order.status === 'pending') {
+                return <div key={order._id}>{ order.number }</div>
+              }
+            })}
         </div>
       </div>
 
@@ -19,7 +34,7 @@ const OrderBoard = () => {
           Выполнено за все время:
         </div>
         <div className="ttext text_type_digits-large">
-          1234
+          { total }
         </div>
       </div>
 
@@ -28,10 +43,9 @@ const OrderBoard = () => {
           Выполнено за сегодня:
         </div>
         <div className="text text_type_digits-large">
-          1234
+          { totalToday }
         </div>
       </div>
-
     </>
   )
 }
